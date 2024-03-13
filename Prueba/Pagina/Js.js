7c35e2c7e86256764b4700548935446f72341d64
sebastian palechor <sebastianhoyos24pale@gmail.com>
2024-03-13 18:26:29 -0500

    prueba

---
+++ Prueba/Pagina/Js.js
@@ -0,0 +1,8 @@
+// Obtener el botón por su id
+var boton = document.getElementById("miBoton");
+
+// Agregar un evento de clic al botón
+boton.addEventListener("click", function() {
+    // Acción a realizar cuando se hace clic en el botón
+    alert("¡Haz hecho clic en el botón!");
+});
